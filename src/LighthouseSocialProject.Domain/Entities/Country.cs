561483abc39299caa6e396665be557630a474a1f
Fırat Alçın <firatalcin04@gmail.com>
2025-09-06 18:49:56 +0300

    Enumeration Bazlı Türlerin Eklenmesi

---
+++ src/LighthouseSocialProject.Domain/Entities/Country.cs
@@ -0,0 +1,14 @@
+﻿namespace LighthouseSocialProject.Domain.Entities;
+
+public class Country
+{
+    public int Id { get;}
+    public string Name { get; }
+    public override string ToString() => Name;
+    internal Country(int id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+    public static Country Create(int id, string name) => new(id, name);
+}
---
+++ src/LighthouseSocialProject.Domain/Enumerations/CameraType.cs
@@ -0,0 +1,27 @@
+﻿using LighthouseSocialProject.Domain.Common;
+
+namespace LighthouseSocialProject.Domain.Enumerations;
+
+public sealed class CameraType : EnumerationBase
+{
+    private CameraType(int id,  string name) : base(id, name) { }
+    
+    public static readonly CameraType SLR = new (1, "SLR");
+    public static readonly CameraType DSLR = new (2, "SLR");
+    public static readonly CameraType Mirrorless = new (3, "SLR");
+    public static readonly CameraType Phone = new (4, "SLR");
+    
+    public static IEnumerable<CameraType> List() => new[] { SLR, DSLR, Mirrorless,  Phone };
+
+    public static CameraType FromId(int id)
+    {
+        return List().FirstOrDefault(x => x.Id == id) 
+               ?? throw new ArgumentOutOfRangeException();
+    }
+
+    public static CameraType FromName(string name)
+    {
+        return List().FirstOrDefault(x => x.Name.Equals(name,StringComparison.OrdinalIgnoreCase)) 
+               ?? throw new ArgumentException($"Invalid camera type {name}");
+    }
+}
